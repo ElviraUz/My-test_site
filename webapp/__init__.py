78d41be6112091aca12bb92367844a8017703fee
ElviraUz <e.uzlovaia@gmail.com>
2020-06-20 03:05:01 +0300

    2

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,36 @@
-from flask import Flask
-
+from flask import Flask, render_template
+#from .forms import LoginForm
 from webapp.model import db
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    @app.route("/")  #декоратор позволяет открывать наш сайт слэш - это локаль
+    def hello():
+        page_title="Киберспорт Арены"
+        return render_template('index.html', page_title=page_title)
+
+    # @app.route('/login')
+    # def login():
+    #     title = "Авторизация"
+    #     login_form = LoginForm()
+    #     return render_template("login.html",page_title=title, form=login_form)
+    
+    @app.route('/arena')
+    def arena():
+        page_title = name()
+        descripton = descripton()
+        metro = metro()
+        website = website()
+        return render_template('testarena.html', page_title=page_title)
+#дописать функция для арены
+
+
+
+    return app
+# if __name__ == 'main':
+#     app = create_app()
+#     app.run()
--- webapp/model.py
+++ webapp/model.py
@@ -1,14 +1,23 @@
-from  flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Arenas(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column (db.Srting, nullable = False) #Название
-    adress = db.Column (db.Srting, nullable = False) #адрес
-    metro = db.Column (db.Srting, nullable = False) #Название
-    description = db.Column (db.Text, nullable = True) #описание
-    website = db.Column(db.Srting, nullable=False)
-    
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String,nullable=False) #Название
+    adress = db.Column(db.String,nullable=False) #адрес
+    metro = db.Column(db.String,nullable=False)  #Название
+    description = db.Column(db.Text,nullable=True) #описание
+    website = db.Column(db.String,nullable=False)
+
     def __repr__(self):
-        return '<Arenas {}'.format(self.name)
+        return '<Arenas {}'.format(self.name)    
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), index=True, unique=True)
+    password = db.Column(db.String(20))
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
