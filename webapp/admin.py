bd2eb77951437577892c2a4c1738624b0dd993fd
ElviraUz <e.uzlovaia@gmail.com>
2020-07-06 01:44:57 +0300

    'adminka1'

--- webapp/admin.py
+++ webapp/admin.py
@@ -2,16 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+import os.path
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///' + os.path.join(basedir, '..', "webapp.db"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/projects/arena/webapp/admin.db'
 app.config['SECRET_KEY'] = 'mysecret'
 
 db = SQLAlchemy(app)
 
+admin = Admin(app)
+
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
-if __name__=='__main__';
-app.run(debug=True)
+
+
+admin.add_view(ModelView(Person, db.session))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
